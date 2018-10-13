9b46d061b68fed803200f08ec3036d54f7ae00b4
Nazmican <nazmicancalik@gmail.com>
2018-10-13 22:28:26 +0300

    Topological sorting fix

--- utils/Util.js
+++ utils/Util.js
@@ -26,6 +26,24 @@ exports.topologicalSort = function(node) {
     for(j = 0; j < sorted_ids.length; j++) {
         res.push(mongoose.Types.ObjectId(sorted_ids[j]));
     }
-    console.log(res);
+ 
+    //var first_element = res.shift();
+    //res.reverse();
+    //res.unshift(first_element);
     return res;
+}
+
+exports.sort = function(nodes,sorted_id_list) {
+    console.log(sorted_id_list);
+    var ret = [];
+    for (var i = 0; i< nodes.length; i++) {
+        ret.push(getElement(nodes,sorted_id_list[i]));
+    }
+
+    return ret;
+}
+
+var getElement = function(list,id) {
+    var elementToReturn = _.find(list,{_id:id});
+    return elementToReturn;
 }
