db0b92b67ec93af778550a1ad17ae622c11f65a8
Nazmican <nazmicancalik@gmail.com>
2018-10-13 21:04:13 +0300

    Topological sorting of nodes

--- controllers/nodeController.js
+++ controllers/nodeController.js
@@ -1,5 +1,6 @@
 // var mongoose = require('mongoose');
 var Node = require('../models/node');
+var Util = require('../utils/Util');
 
 exports.nodes_list = function(req,res) {
     Node.find({}, 'id name')
@@ -19,6 +20,24 @@ exports.node_detail = function(req,res,next) {
               return next(err); 
             }
             //Successful, so send
-            res.send(node_details);
+            var sorted_list = Util.topologicalSort(node_details);
+
+            Node.find({
+                '_id': { $in: sorted_list }
+            }, function(err, docs){
+                res.send(docs);
+            });
+        });
+}
+
+exports.getNode = function(id,callback) {
+    Node.findById(id)
+        .select("-dependencies")
+        .exec(function (err, node_details) {
+            if (err) { 
+              return next(err); 
+            }
+            //Successful, so invoke callback
+            callback(node_details);
         });
 }
--- utils/Util.js
+++ utils/Util.js
@@ -1,6 +1,31 @@
+var TopoSort = require('topo-sort');
+var mongoose = require('mongoose');
+var _ = require('lodash');
+
 var nodeController = require('../controllers/nodeController');
 
 exports.topologicalSort = function(node) {
     var id = node.id;
     var dependencies = node.dependencies;
+
+    var tsort = new TopoSort();
+
+    tsort.add(id,dependencies);
+    
+    var i;
+    for(i = 0; i < dependencies.length; i++) {
+        nodeController.getNode(dependencies[i],function(node){
+            tsort.add(dependencies[i],node);
+        });
+    }
+
+    var sorted_ids = tsort.sort();
+    var res = [];
+
+    var j;
+    for(j = 0; j < sorted_ids.length; j++) {
+        res.push(mongoose.Types.ObjectId(sorted_ids[j]));
+    }
+    console.log(res);
+    return res;
 }
